724a7587f071d719defc4fa126b28478c67eab08
Kim-yongbeom <subum12345@gmail.com>
2021-09-16 22:26:31 +0900

    mysql 진행중

--- ssac_project/ssac_server/controllers/ssac/auth/AuthController.js
+++ ssac_project/ssac_server/controllers/ssac/auth/AuthController.js
@@ -3,27 +3,41 @@ const con = require("../../../modules/mysql");
 const AuthController = {
   uploadSignup: (req, res) => {
     const { id, name, password } = req.body;
-    const sql = "insert into user (name, password) values (?, ?)";
-    const params = [name, password];
+    const sql = "insert into user (id, name, password) values (?, ?, ?)";
+    const params = [id, name, password];
     console.log(req.body);
     con.query(sql, params, (err, result) => {
-      if (id === userIdx) {
+      if (id === id) {
         console.log(err);
         return res.status(400).json({
-          message: "에러가 발생 했습니다",
+          message: "이미 있는 아이디입니다",
         });
       }
 
       res.status(200).json({
         message: "생성이 완료 되었습니다",
-        data: { userIdx, id, name, password },
+        data: result,
       });
     });
   },
 
   uploadSignin: (req, res) => {
     const { id, password } = req.body;
-    const sql = "insert into user select id, password from dual where exists";
+    const sql = "insert into user (id, password) values (?, ?)";
+    const params = [id, password];
+    con.query(sql, params, (err, result) => {
+      if (!result) {
+      }
+    });
+    // const sql = "select * from user where id = ? ";
+    // const params = [id]
+    // con.query(sql, params, (err, result)=>{
+    //   if(!result){
+
+    //   }else{
+
+    //   }
+    // })
   },
 };
 
